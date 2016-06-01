d4a3f44d839dd010083372bf449238ff8632ba73
Carlos C Soto <eclipxe13@gmail.com>
2016-05-31 20:17:07 -0500

    Collection tests with different options

---
+++ tests/GenericCollections/CollectionAllowNullTest.php
@@ -0,0 +1,34 @@
+<?php namespace GenericCollections\Tests;
+
+use GenericCollections\Collection;
+use GenericCollections\Options;
+use GenericCollections\Tests\Samples\Foo;
+
+/**
+ * This only check the Collection methods that behaves
+ * different when the option allow null is set:
+ *
+ * As this is set by the property itself then it only assert that
+ * the option is set and the checktype returns true
+ */
+class CollectionAllowNullTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var Collection */
+    private $collection;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->collection = new Collection(Foo::class, [], Options::ALLOW_NULLS);
+    }
+
+    public function testOptionUniqueValuesIsSet()
+    {
+        $this->assertSame(true, $this->collection->optionAllowNullMembers());
+    }
+
+    public function test()
+    {
+        $this->assertTrue($this->collection->checkElementType(null));
+    }
+}
--- tests/GenericCollections/CollectionEqualTest.php
+++ tests/GenericCollections/CollectionEqualTest.php
@@ -5,41 +5,40 @@ use GenericCollections\Options;
 use GenericCollections\Tests\Samples\Foo;
 
 /**
- * This only check the Set methods that behaves
- * different when the option comparison equal is set
+ * This only check the Collection methods that behaves
+ * different when the option comparison equal is set:
+ *
+ * - contains
+ * - remove
+ * - retainAll
  */
 class CollectionEqualTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var Collection */
+    private $collection;
 
-    /**
-     * Return an empty Collection<Foo>
-     * @return Collection
-     */
-    protected function newFooCollection()
+    protected function setUp()
     {
-        return new Collection(Foo::class, [], Options::COMPARISON_EQUAL);
+        parent::setUp();
+        $this->collection = new Collection(Foo::class, [], Options::COMPARISON_EQUAL);
     }
 
     public function testConstructWithEqual()
     {
-        $col = $this->newFooCollection();
-
-        $this->assertSame(false, $col->optionComparisonIsIdentical());
+        $this->assertSame(false, $this->collection->optionComparisonIsIdentical());
     }
 
     public function testContains()
     {
-        $col = $this->newFooCollection();
-        $col->add(new Foo(123));
+        $this->collection->add(new Foo(123));
 
-        $this->assertTrue($col->contains(new Foo(123)));
-        $this->assertFalse($col->contains(new Foo(0)));
+        $this->assertTrue($this->collection->contains(new Foo(123)));
+        $this->assertFalse($this->collection->contains(new Foo(0)));
     }
 
     public function testRetailAll()
     {
-        $col = $this->newFooCollection();
-        $col->addAll([
+        $this->collection->addAll([
             new Foo(100),
             new Foo(200),
             new Foo(300),
@@ -51,29 +50,28 @@ class CollectionEqualTest extends \PHPUnit_Framework_TestCase
         ];
 
         // return true and reduce the collection to only the expected
-        $this->assertTrue($col->retainAll($expected));
-        $this->assertEquals($expected, $col->toArray());
+        $this->assertTrue($this->collection->retainAll($expected));
+        $this->assertEquals($expected, $this->collection->toArray());
 
         // return false since it didn't change
-        $this->assertFalse($col->retainAll($expected));
+        $this->assertFalse($this->collection->retainAll($expected));
     }
 
     public function testRemove()
     {
-        $col = $this->newFooCollection();
-        $col->addAll([
+        $this->collection->addAll([
             new Foo(200),
             new Foo(200),
             new Foo(200),
         ]);
 
         // item not found
-        $this->assertFalse($col->remove(new Foo(-100)));
+        $this->assertFalse($this->collection->remove(new Foo(-100)));
 
         // item found
-        $this->assertTrue($col->remove(new Foo(200)));
+        $this->assertTrue($this->collection->remove(new Foo(200)));
 
         // the collection must have other 2 items
-        $this->assertCount(2, $col);
+        $this->assertCount(2, $this->collection);
     }
 }
